44013439d26d798d5fefa4726a18262924954261
Conor Dockry <conordockry@gmail.com>
2021-02-01 00:03:41 -0500

    cleanup

--- lib/waldon/tenants/tenant.ex
+++ lib/waldon/tenants/tenant.ex
@@ -11,8 +11,6 @@ defmodule Waldon.Tenants.Tenant do
     field :suffix, :string
     field :full_name, :string
 
-    field :selected, :boolean, virtual: true
-
     many_to_many :leases, Waldon.Leases.Lease, join_through: "tenant_leases"
 
     timestamps()
